6740997fdf1d8bf65beb386a95862a0524c584f1
Dom Harrington <dom@harrington-mail.com>
2015-04-09 09:30:22 +0100

    Add example to git repo

---
+++ example/example.js
@@ -0,0 +1,18 @@
+var gitlog = require('../')
+  , options =
+    { repo: __dirname + '/test-repo-folder'
+    , number: 20
+    , author: 'Dom Harrington'
+    , fields:
+      [ 'hash'
+      , 'abbrevHash'
+      , 'subject'
+      , 'authorName'
+      , 'authorDateRel'
+      ]
+    }
+
+gitlog(options, function(error, commits) {
+  // Commits is an array of commits in the repo
+  console.log(commits)
+})
