25ad9eea7eda41b8aaef32b254f7b86234f8c0d0
Sylvain Malnuit <sylvain.malnuit@lyra-network.com>
2015-04-14 13:49:26 +0200

    feat(git parameters): Manage new parameters ('since', 'after', 'until', 'before', 'commiter')
    feat(export): Export fine name and file status for each commit
    refactor(jshint): Apply jshint rules

--- index.js
+++ index.js
@@ -1,97 +1,123 @@
-module.exports = gitlog
+module.exports = gitlog;
+var exec = require('child_process').exec,
+  extend = require('lodash.assign'),
+  delimiter = '\t',
+  fields = {
+    hash: '%H',
+    abbrevHash: '%h',
+    treeHash: '%T',
+    abbrevTreeHash: '%t',
+    parentHashes: '%P',
+    abbrevParentHashes: '%P',
+    authorName: '%an',
+    authorEmail: '%ae',
+    authorDate: '%ai',
+    authorDateRel: '%ar',
+    committerName: '%cn',
+    committerEmail: '%ce',
+    committerDate: '%cd',
+    committerDateRel: '%cr',
+    subject: '%s'
+  }, notOptFields = [
+    'status', 'files'
+  ];
 
-var exec = require('child_process').exec
-  , extend = require('lodash.assign')
-  // The character to split commit fields by in the custom format
-  , delimiter = '\t'
-  , fields =
-    { hash: '%H'
-    , abbrevHash: '%h'
-    , treeHash: '%T'
-    , abbrevTreeHash: '%t'
-    , parentHashes: '%P'
-    , abbrevParentHashes: '%P'
-    , authorName: '%an'
-    , authorEmail: '%ae'
-    , authorDate: '%ai'
-    , authorDateRel: '%ar'
-    , committerName: '%cn'
-    , committerEmail: '%ce'
-    , committerDate: '%cd'
-    , committerDateRel: '%cr'
-    , subject: '%s'
+/***
+    Add optional parameter to command
+*/
+function addOptional(command, options) {
+  var cmdOptional = ['author', 'since', 'after', 'until', 'before', 'commiter'];
+  for (var i = cmdOptional.length; i--;) {
+    if (options[cmdOptional[i]]) {
+      command += ' --' + cmdOptional[i] + '="' + options[cmdOptional[i]] + '"';
     }
+  }
+}
 
 function gitlog(options, cb) {
-  if (!options.repo) throw new Error('Repo required!')
-  if (!cb) throw new Error('Callback required!')
-
-  var defaultOptions =
-    { number: 10
-    , fields:
-      [ 'abbrevHash'
-      , 'hash'
-      , 'subject'
-      , 'authorName'
-      ]
-    }
-
-  // Set defaults
-  options = extend(defaultOptions, options)
-
-  // Start constructing command
-  var command = 'cd ' + options.repo + ' && git log -n ' + options.number
-
-  if (options.author) {
-    command += ' --author="' + options.author + '"'
+  if (!options.repo) {
+    throw new Error('Repo required!');
+  }
+  if (!cb) {
+    throw new Error('Callback required!');
   }
 
+  var defaultOptions = {
+    number: 10,
+    fields: ['abbrevHash', 'hash', 'subject', 'authorName']
+  };
+
+  // Set defaults
+  options = extend(defaultOptions, options);
+
+  // Start constructing command
+  var command = 'cd ' + options.repo + ' && git log -n ' + options.number;
+
+  addOptional(command, options);
+
   // Start of custom format
-  command += ' --pretty="'
+  command += ' --pretty="@begin@';
 
   // Iterating through the fields and adding them to the custom format
   options.fields.forEach(function(field) {
-    if (!fields[field]) throw new Error('Unknown field: ' + field)
-    command += delimiter + fields[field]
-  })
+    if (!fields[field] && field.indexOf(notOptFields) === -1) throw new Error('Unknown field: ' + field);
+    command += delimiter + fields[field];
+  });
 
   // Close custom format
-  command += '"'
+  command += '@end@"';
 
   // Append branch if specified
   if (options.branch) {
-    command += ' ' + options.branch
+    command += ' ' + options.branch;
   }
 
   if (options.file) {
-    command += ' -- ' + options.file
+    command += ' -- ' + options.file;
   }
 
+  //File and fiel status
+  command += ' --name-status';
+
   exec(command, function(err, stdout, stderr) {
-    var commits = stdout.split('\n')
+    var commits = stdout.split('\n@begin@');
 
     // Remove the last blank element from the array
-    commits.pop()
+    commits.pop();
 
-    commits = parseCommits(commits, options.fields)
+    commits = parseCommits(commits, options.fields);
 
-    cb(stderr || err, commits)
-  })
+    cb(stderr || err, commits);
+  });
 }
 
 function parseCommits(commits, fields) {
   return commits.map(function(commit) {
-    commit = commit.split(delimiter)
+    var parts = commit.split('@end@\n\n');
+    commit = parts[0].split(delimiter);
+
+    if (parts[1]) {
+      commit = commit.concat(parts[1].replace(/\n/g, '\t').split(delimiter));
+    }
 
     // Remove the first empty char from the array
-    commit.shift()
+    commit.shift();
 
-    var parsed = {}
+    var parsed = {};
 
     commit.forEach(function(commitField, index) {
-      parsed[fields[index]] = commitField
-    })
+      if (fields[index]) {
+        parsed[fields[index]] = commitField;
+      } else {
+        var pos = index % notOptFields.length;
 
-    return parsed
-  })
+        if (!parsed[notOptFields[pos]]) {
+          parsed[notOptFields[pos]] = [];
+        }
+        parsed[notOptFields[pos]].push(commitField);
+      }
+    });
+
+    return parsed;
+  });
 }
